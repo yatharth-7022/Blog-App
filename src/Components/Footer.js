e56c0b7b871459ef95ac95ad14cf98added5b015
yatharth-7022 <yatharth7022@gmail.com>
2024-11-26 14:23:23 +0530

    added node to git ignore

---
+++ src/Components/Footer.js
@@ -0,0 +1,4 @@
+function Footer() {
+  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+}
+export default Footer;
---
+++ src/Components/Results.js
@@ -0,0 +1,4 @@
+function Results({ posts }) {
+  return <p>🚀 {posts.length} atomic posts found</p>;
+}
+export default Results;
